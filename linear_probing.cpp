3f8b872aee0ac1c9a1940321a2059029e73150a7
Sahil <80311646+Sahil-Bhor@users.noreply.github.com>
2022-04-14 14:00:31 +0530

    Collision resolution M2

---
+++ linear_probing.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#define size 10
+using namespace std;
+
+int hash_func(int num)
+{
+    int index = num % size;
+    return index;
+}
+
+void simpleHashTable(int arr[], int index, int val)
+{
+    int arr1 = *arr;
+    if (arr[index] == 0)
+    {
+        arr[index] = val;
+    }
+    else
+    {
+        if (index == 9)
+        {
+            index = 0;
+            simpleHashTable(arr, index, val);
+            return;
+        }
+        index += 1;
+        simpleHashTable(arr, index, val);
+    }
+}
+
+void set_data(int arr[])
+{
+    int no, num, index;
+
+    cout << "How many nos you want to insert : ";
+    cin >> no;
+    for (int i = 0; i < no; i++)
+    {
+        cout << "Enter the number : ";
+        cin >> num;
+        index = hash_func(num);
+        simpleHashTable(arr, index, num);
+    }
+}
+
+void print_hash(int arr[])
+{
+    cout << "Hash Table : \n";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << "\n";
+    }
+}
+
+int main()
+{
+    int arr[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = 0;
+    }
+
+    set_data(arr);
+    print_hash(arr);
+
+    cout << endl;
+    return 0;
+}
